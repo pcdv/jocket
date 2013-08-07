cd6aee5ad26f2d29751b9af311e0a38f4f3de268
pcdv <paulcdv+eclipse@gmail.com>
2013-08-07 09:25:36 +0200

    Minimize access to rseq() etc.

--- src/jocket/impl/JocketWriter.java
+++ src/jocket/impl/JocketWriter.java
@@ -5,7 +5,8 @@ import java.nio.ByteBuffer;
 public class JocketWriter extends AbstractJocketImpl {
 
 	/**
-	 * The sequence number of the next packet to write.
+	 * The sequence number of the next packet to write. Accessed only by the
+	 * writer thread.
 	 */
 	private int wseq;
 
@@ -14,45 +15,52 @@ public class JocketWriter extends AbstractJocketImpl {
 	}
 
 	public int write(byte[] data, int off, int len) {
-		int rseq = getReaderPacket();
-
-		// cannot write if all packets are written and the reader didn't read any
+		final int rseq = rseq();
+		// cannot write if all packets are written and the reader didn't read them
 		if (wseq - rseq >= npackets)
 			return 0;
 
-		int infoIndex = wseq & packetMask;
-		int writable = Math.min(getAvailableSpace(), len);
-		final int head = head();
+		final int head = head(rseq);
+		final int writable = Math.min(getAvailableSpace(rseq, head), len);
 		if (writable > 0) {
-			int start = head & dataMask;
-			buf.putInt(PACKET_INFO + infoIndex * LEN_PACKET_INFO, start);
-			buf.putInt(PACKET_INFO + infoIndex * LEN_PACKET_INFO + 4, writable);
-			buf.position(start);
+			final int idx = wseq & packetMask;
+			buf.putInt(PACKET_INFO + idx * LEN_PACKET_INFO, head);
+			buf.putInt(PACKET_INFO + idx * LEN_PACKET_INFO + 4, writable);
+			buf.position(head);
 			buf.put(data, off, writable);
 			buf.putInt(WSEQ, ++wseq);
 		}
 		return writable;
 	}
 
-	private int head() {
-		if (wseq == buf.getInt(RSEQ))
+	/**
+	 * Returns the offset where next data can be written.
+	 * 
+	 * @param rseq
+	 */
+	private int head(int rseq) {
+		// if all packets are read, we can start at 0
+		if (wseq == rseq)
 			return 0;
 
-		int windex = (wseq - 1) & packetMask;
+		final int idx = (wseq - 1) & packetMask;
 
-		return buf.getInt(PACKET_INFO + windex * LEN_PACKET_INFO)
-		    + buf.getInt(PACKET_INFO + windex * LEN_PACKET_INFO + 4);
+		return buf.getInt(PACKET_INFO + idx * LEN_PACKET_INFO)
+		    + buf.getInt(PACKET_INFO + idx * LEN_PACKET_INFO + 4);
 	}
 
-	private long tail() {
-		int rseq = buf.getInt(RSEQ);
-		int rindex = rseq & packetMask;
-		return buf.getInt(PACKET_INFO + rindex * LEN_PACKET_INFO);
+	private long start(int packet) {
+		return buf.getInt(PACKET_INFO + (packet & packetMask) * LEN_PACKET_INFO);
 	}
 
-	private int getAvailableSpace() {
-		final int head = head();
-		int space = (int) (capacity - (head - tail()));
+	/**
+	 * Returns how many bytes can be written in one chunk at current position.
+	 * 
+	 * @param rseq current seqnum of the reader
+	 * @param head current data head
+	 */
+	private int getAvailableSpace(int rseq, int head) {
+		int space = (int) (capacity - (head - start(rseq)));
 		if (space <= 0)
 			return 0;
 
@@ -63,15 +71,18 @@ public class JocketWriter extends AbstractJocketImpl {
 		return space;
 	}
 
-	private int getReaderPacket() {
+	private int rseq() {
 		return buf.getInt(RSEQ);
 	}
 
+	/**
+	 * Returns how many bytes can be written (mainly for test purposes).
+	 */
 	public int available() {
 		// cannot write if all packets are written and the reader didn't read any
-		int rseq = getReaderPacket();
+		int rseq = rseq();
 		if (wseq - rseq >= npackets)
 			return 0;
-		return getAvailableSpace();
+		return getAvailableSpace(rseq, head(rseq));
 	}
 }
