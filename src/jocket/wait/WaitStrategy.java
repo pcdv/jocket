e7283415d25c58a40fe5a26476170e7c25d02251
pcdv <paulcdv+eclipse@gmail.com>
2013-08-22 07:17:34 +0200

    cleanup

--- src/jocket/wait/WaitStrategy.java
+++ src/jocket/wait/WaitStrategy.java
@@ -1,9 +1,22 @@
 package jocket.wait;
 
+/**
+ * Strategy used to wait for some time when data could not be read or written.
+ * Currently used only in JockectInputStream and JockectOutputStream wrappers.
+ * 
+ * @author pcdv
+ */
 public interface WaitStrategy {
 
+	/**
+	 * Pauses for some time.
+	 */
 	void pause();
 
+	/**
+	 * Resets the strategy. Called when data has been read or written
+	 * successfully.
+	 */
 	void reset();
 
 }
