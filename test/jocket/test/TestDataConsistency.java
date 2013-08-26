3629459b4a6ac1aef4be1eed7395122ff5e57247
pcdv <pcdv@server.fake>
2013-08-26 09:27:48 +0200

    new junit

--- test/jocket/test/TestDataConsistency.java
+++ test/jocket/test/TestDataConsistency.java
@@ -1,24 +1,68 @@
 package jocket.test;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Random;
+
 import org.junit.Test;
 
-public class TestDataConsistency {
+public class TestDataConsistency extends AbstractJocketSocketTest {
 
-  @Before
-  public void setUp() throws Exception {
-  }
-
-  @After
-  public void tearDown() throws Exception {
-  }
+  Random random = new Random();
 
   @Test
   public void test() {
-    fail("Not yet implemented");
+    final byte[] sequence = genRandomSequence();
+
+    new Thread("Writer") {
+      @Override
+      public void run() {
+        OutputStream out = s.getOutputStream();
+        try {
+          for (int pos = 0; pos < sequence.length;) {
+            int len = Math.min(random.nextInt(500), sequence.length - pos);
+            out.write(sequence, pos, len);
+            if (random.nextInt() % 3 == 0)
+              out.flush();
+            pos += len;
+          }
+          out.flush();
+          // out.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }.start();
+
+    byte[] buf = new byte[100];
+    ByteArrayOutputStream bout = new ByteArrayOutputStream(sequence.length);
+
+    while (bout.size() < sequence.length) {
+      try {
+        int len = c.getInputStream().read(buf);
+        if (len < 0)
+          break;
+        bout.write(buf, 0, len);
+      } catch (IOException e) {
+        e.printStackTrace();
+        break;
+      }
+    }
+
+    assertEquals(sequence.length, bout.size());
+    assertTrue(Arrays.equals(sequence, bout.toByteArray()));
   }
 
+  private byte[] genRandomSequence() {
+    byte[] res = new byte[20000000];
+    for (int i = 0; i < res.length; i++)
+      res[i] = (byte) random.nextInt(256);
+
+    return res;
+  }
 }
