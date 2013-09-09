f0848e3833c68f922af5da189a2b4c367eed4bf0
pcdv <paulcdv+github@gmail.com>
2013-09-09 15:04:52 +0200

    Code shrinking

--- test/jocket/bench/BenchClient.java
+++ test/jocket/bench/BenchClient.java
@@ -14,25 +14,11 @@ import jocket.net.JocketSocket;
  *
  * @author pcdv
  */
-public final class BenchClient {
-
-  static final int REPS = Integer.getInteger("reps", 300000);
-
-  static final int REPLY_SIZE = Integer.getInteger("replySize", 1024);
-
-  static final int BATCH = Integer.getInteger("batch", 1);
-
-  static final boolean USE_JOCKET = !Boolean.getBoolean("tcp");
-
-  static final int PORT = Integer.getInteger("port", 3333);
-
-  static final int WARMUP = Integer.getInteger("warmup", 50000);
+public final class BenchClient implements Settings {
 
   private final byte[] buf;
 
   private final long[] nanos;
-
-  private final String fileName;
 
   private DataInputStream in;
 
@@ -41,26 +27,18 @@ public final class BenchClient {
   public BenchClient() throws IOException {
     this.nanos = new long[REPS];
     this.buf = new byte[REPLY_SIZE];
-    if (USE_JOCKET)
-      this.fileName = initWithJocket();
-    else
-      this.fileName = initWithSocket();
-  }
-
-  private String initWithJocket() throws IOException {
-    JocketSocket s = new JocketSocket(PORT);
-    in = new DataInputStream(s.getInputStream());
-    out = new DataOutputStream(s.getOutputStream());
-    return "/tmp/Jocket";
-  }
-
-  @SuppressWarnings("resource")
-  private String initWithSocket() throws IOException {
-    Socket s = new Socket("localhost", PORT);
-    s.setTcpNoDelay(true);
-    in = new DataInputStream(s.getInputStream());
-    out = new DataOutputStream(s.getOutputStream());
-    return "/tmp/Socket";
+    if (USE_JOCKET) {
+      JocketSocket s = new JocketSocket(PORT);
+      in = new DataInputStream(s.getInputStream());
+      out = new DataOutputStream(s.getOutputStream());
+    }
+    else {
+      @SuppressWarnings("resource")
+      Socket s = new Socket("localhost", PORT);
+      s.setTcpNoDelay(true);
+      in = new DataInputStream(s.getInputStream());
+      out = new DataOutputStream(s.getOutputStream());
+    }
   }
 
   /**
@@ -85,23 +63,22 @@ public final class BenchClient {
     }
     time = System.currentTimeMillis() - time;
 
-    System.out.printf("Done in %dms. Dumping results in %s\n", time, fileName);
-    dumpResults(fileName);
+    System.out.printf("Done in %dms. Dumping results in %s\n",
+                      time,
+                      OUTPUT_FILE);
+    dumpResults(OUTPUT_FILE);
   }
 
   public void iter(int batch) throws IOException {
     for (int i = 0; i < batch; i++) {
-      // send request
       out.write(buf, 0, 4);
       out.flush();
-
-      // read response
       in.readFully(buf, 0, REPLY_SIZE);
     }
   }
 
   private void dumpResults(String fileName) throws IOException {
-    PrintWriter w = new PrintWriter(new FileOutputStream(fileName));
+    PrintWriter w = new PrintWriter(new FileOutputStream("/tmp/" + fileName));
     for (int i = 0; i < nanos.length; i++)
       w.println(nanos[i] / 1000.0);
     w.close();
---
+++ test/jocket/bench/Settings.java
@@ -0,0 +1,18 @@
+package jocket.bench;
+
+public interface Settings {
+
+  int REPS = Integer.getInteger("reps", 300000);
+
+  int REPLY_SIZE = Integer.getInteger("replySize", 1024);
+
+  int BATCH = Integer.getInteger("batch", 1);
+
+  boolean USE_JOCKET = !Boolean.getBoolean("tcp");
+
+  int PORT = Integer.getInteger("port", 3333);
+
+  int WARMUP = Integer.getInteger("warmup", 50000);
+
+  String OUTPUT_FILE = USE_JOCKET ? "Jocket" : "Socket";
+}
